a7e574f890a77af1b5476a2065508ec05a2756d1
Suman Jha <meetsumanat@gmail.com>
2020-05-02 20:39:30 -0700

    try out for graph mongo

---
+++ apps/api/src/app/items/items.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { ItemsService } from './services/items.service';
+import { ItemsResolver } from './resolvers/items.resolver';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ItemSchema } from './item.schema';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: 'Item', schema: ItemSchema }])
+  ],
+  providers: [ItemsService, ItemsResolver]
+})
+export class ItemsModule {}
